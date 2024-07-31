781df18f33f2b4059f8a1fab4e9ae33a209c1575
Gulsah <gulsharslan@gmail.com>
2024-07-31 17:09:49 +0300

    ..

---
+++ Core/RentACar.Application/Features/Mediator/Handlers/TestimonialHandlers/CreateTestimonialCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using RentACar.Application.Features.Mediator.Commands.TestimonialCommands;
+using RentACar.Application.Interfaces;
+using RentACar.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Application.Features.Mediator.Handlers.TestimonialHandlers
+{
+    public class CreateTestimonialCommandHandler : IRequestHandler<CreateTestimonialCommand>
+    {
+        private readonly IRepository<Testimonial> _repository;
+        public CreateTestimonialCommandHandler(IRepository<Testimonial> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(CreateTestimonialCommand request, CancellationToken cancellationToken)
+        {
+            await _repository.CreateAsync(new Testimonial
+            {
+                Name = request.Name,
+                Comment = request.Comment,
+                ImageUrl = request.ImageUrl,
+                Title = request.Title
+            });
+        }
+    }
+}
---
+++ Frontends/RentACar.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACar.Dto.ContactDtos;
+using System.Text;
+
+namespace RentACar.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateContactDto createContactDto)
+        {
+            var client=_httpClientFactory.CreateClient();
+            createContactDto.SendDate = DateTime.Now;
+            var jsonData = JsonConvert.SerializeObject(createContactDto);
+            StringContent content = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7055/api/Contacts",content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+            return View();
+        }
+    }
+}
