ddf34a1de251c739858245f0ee3280151adfb780
Gulsah <gulsharslan@gmail.com>
2024-08-01 23:13:12 +0300

    ..

--- Infrastructure/RentACar.Persistence/Repositories/CarRepositories/CarRepository.cs
+++ Infrastructure/RentACar.Persistence/Repositories/CarRepositories/CarRepository.cs
@@ -24,5 +24,11 @@ namespace RentACar.Persistence.Repositories.CarRepositories
             var values = _context.Cars.Include(x => x.Brand).ToList();
             return values;
         }
+
+        public List<Car> GetLast5CarsWithBrand()
+        {
+            var values=_context.Cars.Include(x => x.Brand).OrderByDescending(y=>y.CarID).Take(5).ToList();
+            return values;
+        }
     }
 }
